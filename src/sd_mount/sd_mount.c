7664ba24d6f20b656127fe48d4f233e189c74946
fdai7060 <94118331+Youmie34@users.noreply.github.com>
2026-06-25 17:59:30 +0200

    reads from card and lists files

--- src/sd_mount/sd_mount.c
+++ src/sd_mount/sd_mount.c
@@ -1,7 +1,33 @@
 #include "sd_mount.h"
 
+DIR *sd_dir = NULL;
+
 esp_vfs_fat_mount_config_t sd_mount_config = {
     .format_if_mount_failed = false,
     .max_files = 10,
     .allocation_unit_size = 16 * 1024,
 };
+
+void open_directory()
+{
+    sd_dir = opendir(MOUNT_PATH);
+
+    if (sd_dir == NULL)
+    {
+        printf("Failed to open directory: %s\n", MOUNT_PATH);
+        return;
+    }
+}
+
+void list_files()
+{
+    if (sd_dir != NULL)
+    {
+        struct dirent *entry;
+        while ((entry = readdir(sd_dir)) != NULL)
+        {
+            printf("%s\n", entry->d_name);
+        }
+        closedir(sd_dir);
+    }
+}
--- src/sd_mount/sd_mount.h
+++ src/sd_mount/sd_mount.h
@@ -2,9 +2,15 @@
 #define SD_MOUNT_H
 
 #include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+#include <dirent.h>
 
 #define MOUNT_PATH "/sdcard"
 
 extern esp_vfs_fat_mount_config_t sd_mount_config;
+extern DIR *sd_dir;
+
+void open_directory();
+void list_files();
 
 #endif // SD_MOUNT_H
